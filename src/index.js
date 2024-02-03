f702481671ccf651b09028510d7dce48afa90cd6
amag001 <1092712827@qq.com>
2024-02-03 16:00:59 +0800

    安装craco来修改webpack的配置，webpack默认为黑盒看不到

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import { sum } from "@/test.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(sum(5, 7));
 root.render(
   // <React.StrictMode>
   <App />
