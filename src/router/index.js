094c4318e787c11334b42c57e0cdab0206e0216b
amag001 <1092712827@qq.com>
2024-03-13 16:00:37 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,52 @@
+import { Suspense, lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "@pages/Layout/index";
-import New from "../pages/New";
-import Month from "../pages/Month";
-import Year from "../pages/Year";
+// import Layout from "@pages/Layout/index";
+// import New from "../pages/New";
+// import Month from "../pages/Month";
+// import Year from "../pages/Year";
+
+// 路由懒加载
+/**
+ * 1，使用lazy函数对组件进行导入
+ * 2，对组件使用Suspense包裹,fallback属性为组件未加载出来的展示。
+ */
+const Layout = lazy(() => import("@pages/Layout/index"));
+const New = lazy(() => import("@pages/New"));
+const Month = lazy(() => import("@pages/Month"));
+const Year = lazy(() => import("@pages/Year"));
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout></Layout>,
+    element: (
+      <Suspense fallback={"Loading..."}>
+        <Layout></Layout>
+      </Suspense>
+    ),
     children: [
       {
         index: true,
-        element: <Month></Month>,
+        element: (
+          <Suspense>
+            <Month></Month>
+          </Suspense>
+        ),
       },
       {
         path: "/month",
-        element: <Month></Month>,
+        element: (
+          <Suspense>
+            <Month></Month>
+          </Suspense>
+        ),
       },
       {
         path: "/year",
-        element: <Year></Year>,
+        element: (
+          <Suspense>
+            <Year></Year>
+          </Suspense>
+        ),
       },
     ],
   },
